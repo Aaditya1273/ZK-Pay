742fbbdad9ce1b01989eaf4404bde81ef3a7a619
Aaditya1273 <aadityaiitian1515@gmail.com>
2026-07-15 07:31:53 +0530

    neeted

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  images: {
+    domains: ["web3.okx.com"],
+  },
+};
+
+export default nextConfig;
